e4499a74e906a0677db5bd33faa65cc7cb93d59b
sonechka-sys <sonechkachekanova@gmail.com>
2025-12-11 18:50:14 +0200

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,192 @@
+body{
+font-family: ’Roboto’, sans-serif;
+margin: 0;
+padding: 16px;
+background-color: #eaeaea;
+color: #333;
+}
+.section-main{
+padding-top: 40px;
+padding-bottom: 120px;
+background-color: #556b2f;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+}
+.section-main-logo{
+width: 160px;
+}
+.section-main-title{
+margin-top: 40px;
+margin-bottom: 20px;
+color: #fff;
+font-family: ’Lora’, sans-serif;
+font-size: 50px;
+font-weight: 700;
+}
+.section-main-subtitle {
+margin-bottom: 40px;
+color: #fff;
+font-size: 22px;
+}
+.section-main-button {
+background-color: #8fbc8f;
+color: #fff;
+font-weight: bold;
+text-transform: uppercase;
+padding: 12px 30px;
+cursor: pointer;
+border-style: none;
+border-radius: 25px;
+}
+.second-section {
+width: 100%;
+display: flex; 
+justify-content: space-evenly;
+align-items: center;
+padding-top: 100px;
+padding-bottom: 100px;
+}
+.second-section-img{
+width: 500px;
+border: 5px solid #556b2f;
+border-radius: 25px
+}
+.second-section-info{
+margin-left: 30px;
+max-width: 600px;
+}
+.section-subtitle{
+margin-top: 0px;
+margin-bottom: 10px;
+color: #8fbc8f;
+font-size: 18px;
+font-weight: bold;
+text-transform: uppercase;
+}
+.section-title{
+margin-top: 0px;
+margin-bottom: 40px;
+color: #556b2f;
+font-size: 32px;
+font-weight: bold;
+text-transform: capitalize;
+}
+.section-section-text{
+margin: 30px;
+color: #666;
+font-size: 18px;
+}
+.section-section-name{
+color: #556b2f;
+text-transform: capitalize;
+}
+.section-section-name2{
+color: #8fbc8f;
+font-weight: bold;
+text-transform: uppercase;
+}
+.third-section{
+padding-top: 100px;
+padding-bottom: 100px;
+background-color: #f5f5f5;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+}
+.third-section-books{
+display: flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
+margin-top: 50px;
+margin-buttom: 50px;
+}
+.third-section-book{
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: 10px 20px;
+}
+.book-img{
+height: 200px;
+}
+.book-title{
+margin-top: 10px;
+margin-bottom: 5px;
+color: #556b2f;
+font-size: 22px;
+font-weight: bold;
+}
+.book-author{
+margin-top: 2px;
+margin-bottom: 2px;
+color: #666;
+}
+.book-price{
+margin: 0;
+color: #8fbc8f;
+font-size: 20px;
+font-weight: bold;
+}
+.footer{
+padding-top: 100px;
+padding-bottom: 100px;
+background-color: #2f4f4f;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+}
+.footer-title{
+color: #fff;
+}
+.footer-text{
+width: 600px;
+text-align: center;
+margin-top: 20px;
+margin-bottom: 20px;
+color: #8fbc8f;
+font-size: 20px;
+}
+.footer-text-main{
+color: #fff;
+}
+.footer-form{
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+}
+.form-title{
+margin-top: 20px;
+margin-bottom: 10px;
+text-align: center;
+color: #fff;
+font-weight: bold;
+text-transform: uppercase;
+}
+.form-input{
+width: 400px;
+height: 40px;
+margin-top: 20px;
+margin-bottom: 10px;
+padding: 0px 10px;
+background-color: #2f4f4f;
+color: #fff;
+border: 1px solid #fff;
+border-radius: 8px;
+}
+.footer-button{
+margin-top: 30px;
+background-color: #8fbc8f;
+color: #fff;
+font-weight: bold;
+padding: 10px 20px;
+cursor: pointer;
+border-style: none;
+border-radius: 25px;
+}
